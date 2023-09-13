ac4f684f6e0333b9bdafcc6210d0160b26b4b074
Ndubuisi William <princewill835@gmail.com>
2023-09-13 13:48:24 -0700

    authorize user control added to controller

--- backend/routes/controllers/userControllers.js
+++ backend/routes/controllers/userControllers.js
@@ -1,11 +1,11 @@
 // @desc Auth user/set token
 // route POST /api/user/auth
 // @access public
-import asyncHandler from "express-async-handler"
-const authUser = (req, res) => {
-    res.status(200).json({ message: "Auth User"})
-}
+import asyncHandler from "express-async-handler";
+const authUser = asyncHandler(async (req, res) => {  
+	res.status(200).json({ message: "Auth User" });
+	res.status(401);
+	throw new Error("Something went wrong");
+});
 
-export{
-    authUser
-}
+export { authUser }; 
